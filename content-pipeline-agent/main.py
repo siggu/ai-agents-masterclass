45d994c95640721837e89ef3ba9c8f5ac8f2d89d
siggu <siggu100@gmail.com>
2025-09-15 15:21:36 +0900

    feat: first flow

--- content-pipeline-agent/main.py
+++ content-pipeline-agent/main.py
@@ -1,6 +1,50 @@
-def main():
-    print("Hello from content-pipeline-agent!")
+from crewai.flow.flow import Flow, and_, listen, or_, router, start
+from pydantic import BaseModel
 
 
-if __name__ == "__main__":
-    main()
+class MyFirstFlowState(BaseModel):
+
+    user_id: int = 1
+    is_admin: bool = False
+
+
+class MyFirstFlow(Flow[MyFirstFlowState]):
+
+    @start()
+    def first(self):
+        print(self.state.user_id)
+        print("Hello")
+
+    @listen(first)
+    def second(self):
+        self.state.user_id = 2
+        print("world")
+
+    @listen(first)
+    def third(self):
+        print("!")
+
+    @listen(and_(second, third))
+    def final(self):
+        print(":)")
+
+    @router(final)
+    def route(self):
+        if self.state.is_admin:
+            return "even"
+        else:
+            return "odd"
+
+    @listen("even")
+    def handle_even(self):
+        print("even")
+
+    @listen("odd")
+    def handle_odd(self):
+        print("odd")
+
+
+flow = MyFirstFlow()
+
+flow.plot()
+flow.kickoff()
