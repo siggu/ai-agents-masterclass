3614d94106815c836c659ae516bce68f320fec67
siggu <siggu100@gmail.com>
2025-09-18 18:11:45 +0900

    feat: streamlit data flow

--- chatbot-agent/main.py
+++ chatbot-agent/main.py
@@ -1,49 +1,14 @@
-import time
-
 import streamlit as st
 
-st.header("Hello world!")
+if "is_admin" not in st.session_state:
+    st.session_state["is_admin"] = False
 
-st.button("Click me please")
+st.header("Hello!")
 
-st.text_input(
-    "Write your API Key",
-    max_chars=20,
-)
+name = st.text_input("What is your name?")
 
-st.feedback("faces")
+if name:
+    st.write(f"Hello {name}")
+    st.session_state["is_admin"] = True
 
-with st.sidebar:
-    st.badge("Badge 1")
-
-
-tab1, tab2, tab3 = st.tabs(["Agent", "Chat", "Output"])
-
-with tab1:
-    st.header("Agent 1")
-with tab2:
-    st.header("Agent 2")
-with tab3:
-    st.header("Agent 3")
-
-
-time.sleep(2)
-with st.chat_message("human"):
-    st.text("안녕! 지금 한국의 날씨는 어때?")
-
-time.sleep(3)
-with st.chat_message("ai"):
-    st.text("...")
-    with st.status("Agent is using tool") as status:
-        time.sleep(2)
-        status.update(label="Agent is searching the web")
-        time.sleep(2)
-        status.update(label="Agent is reading the web")
-        time.sleep(3)
-        status.update(label="30도")
-        status.update(state="complete")
-
-st.chat_input(
-    "Write a message for the assistant.",
-    accept_file=True,
-)
+print(st.session_state["is_admin"])
