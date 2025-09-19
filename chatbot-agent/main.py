e6af51b927264fd32e0f7f1fd986278c25832217
siggu <siggu100@gmail.com>
2025-09-19 18:31:48 +0900

    feat: chat ui

--- chatbot-agent/main.py
+++ chatbot-agent/main.py
@@ -1,14 +1,53 @@
+import dotenv
+
+dotenv.load_dotenv()
+import asyncio
+
 import streamlit as st
+from agents import Agent, Runner, SQLiteSession
 
-if "is_admin" not in st.session_state:
-    st.session_state["is_admin"] = False
+if "agent" not in st.session_state:
+    st.session_state["agent"] = Agent(
+        name="Chatbot agent",
+        instructions="""
+        You are a helpful assistant.
+        """,
+    )
+agent = st.session_state["agent"]
 
-st.header("Hello!")
+if "session" not in st.session_state:
+    st.session_state["session"] = SQLiteSession(
+        "chat-history",
+        "chatbot-memory.db",
+    )
 
-name = st.text_input("What is your name?")
+session = st.session_state["session"]
 
-if name:
-    st.write(f"Hello {name}")
-    st.session_state["is_admin"] = True
 
-print(st.session_state["is_admin"])
+async def run_agent(message):
+    stream = Runner.run_streamed(
+        agent,
+        message,
+        session=session,
+    )
+
+    async for event in stream.stream_events():
+        if event.type == "raw_response_event":
+            if event.data.type == "response.output_text.delta":
+                with st.chat_message("ai"):
+                    st.write(event.data.delta)
+
+
+prompt = st.chat_input("Write a message for your assistant")
+
+if prompt:
+    with st.chat_message("human"):
+        st.write(prompt)
+    asyncio.run(run_agent(prompt))
+
+
+with st.sidebar:
+    reset = st.button("Reset memory")
+    if reset:
+        asyncio.run(session.clear_session())
+    st.write(asyncio.run(session.get_items()))
